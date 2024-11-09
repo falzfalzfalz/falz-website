59cd67a451a62ff6e492d805581408d9362afd5e
falz <oscar@softwhale.be>
2024-11-09 18:48:39 +0800

    feat: Replace Netlify adapter with Cloudflare and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
@@ -10,9 +9,12 @@ import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 import svelte from "@astrojs/svelte";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://falz.studio/",
+
   integrations: [
     sitemap(),
     robotsTxt({
@@ -26,12 +28,16 @@ export default defineConfig({
     icon(),
     svelte(),
   ],
+
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
+
   output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
+
   vite: {
     assetsInclude: "**/*.riv",
   },
+
+  adapter: cloudflare(),
 });
