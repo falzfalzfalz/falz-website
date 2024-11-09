12386bce64c0aba7a54f8e76e3ceff0105bdf65c
falz <oscar@softwhale.be>
2024-11-09 17:02:04 +0800

    fix: Update site URL and sitemap links to reflect new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
+  site: "https://falz.studio/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
+        "https://falz.studio/sitemap-index.xml",
+        "https://falz.studio/sitemap-0.xml",
       ],
     }),
     solidJs(),
